3577d33d314ebd1b31732882ee0549b41cd8d7f4
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-02-27 20:13:20 +0100

    fix: add comment

--- src/testAdapter.ts
+++ src/testAdapter.ts
@@ -1,6 +1,10 @@
 import { TestAdapter } from 'botbuilder'
 import { Adapter } from './adapter'
 
+/**
+ * creates a adapter for testing
+ * @param adapter
+ */
 export function createTestAdapter(adapter: Adapter) {
   return new TestAdapter(adapter.onTurn)
 }
